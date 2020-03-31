6c0949feded67e469463479d5d4d460b76b98ed9
Kristina-Pianykh <pianykh@shh.mpg.de>
2020-03-31 15:58:25 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Othanieljen
+# Jen Cluster Comparative Wordlist
+
+Cite the source dataset as
+
+> Othaniel, Nlabephee Kefas. 2017. A phonological comparative study of the Jen language cluster. (MA thesis, Jos: Theological College of Northern Nigeria; 1–83pp.)
+
+
+Conceptlists in Concepticon:
+- [Othaniel-2017-300](https://concepticon.clld.org/contributions/Othaniel-2017-300)
+## Statistics
+
+
+[![Build Status](https://travis-ci.org/lexibank/othanieljen.svg?branch=master)](https://travis-ci.org/lexibank/othanieljen)
+![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
+![Concepticon: 95%](https://img.shields.io/badge/Concepticon-95%25-green.svg "Concepticon: 95%")
+![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
+
+- **Varieties:** 10
+- **Concepts:** 300
+- **Lexemes:** 2,999
+- **Sources:** 1
+- **Synonymy:** 1.00
---
+++ lexibank_othanieljen.py
@@ -0,0 +1,33 @@
+import pylexibank
+from clldutils.path import Path
+from clldutils.misc import slug
+
+
+class Dataset(pylexibank.Dataset):
+    dir = Path(__file__).parent
+    id = "othaniel2017"
+
+    form_spec = pylexibank.FormSpec(
+        brackets={"(": ")"},
+        missing_data=('', ' '),
+        strip_inside_brackets=True
+    )
+
+    def cmd_makecldf(self, args):
+        data = self.raw_dir.read_csv('raw.csv', dicts=True)
+        args.writer.add_sources()
+        languages = args.writer.add_languages(lookup_factory="Name")
+
+        concepts = args.writer.add_concepts(
+            id_factory=lambda c: c.id.split("-")[-1] + "_" + slug(c.english), lookup_factory="Name"
+        )
+
+        for row in pylexibank.progressbar(data):
+            for language, lexeme in row.items():
+                if language in languages:
+                    args.writer.add_forms_from_value(
+                        Language_ID=languages[language],
+                        Parameter_ID=concepts[row["Gloss"]],
+                        Value=lexeme,
+                        Source="Othaniel2017",
+                    )
